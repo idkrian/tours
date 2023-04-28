2231a6ea21d422d115b117c80d7507d7f1c1fb03
Rian <riansouza0@gmail.com>
2023-04-28 00:09:29 -0300

    feat(app): Project creation

---
+++ app.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req,res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
+app.post('/api/v1/tours', (req,res) => {
+    const newId = tours[tours.length -1].id + 1
+    const newTour = {id: newId, ...req.body}
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data:{
+                tour: newTour
+            }
+        })
+    })
+
+})
+
+app.post('/', (req,res) => {
+    res.send()
+})
+
+app.listen(port, (req,res) => {
+    console.log(`Running on port ${port}`)
+})
